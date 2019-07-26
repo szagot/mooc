b1ed1bb610fd9e2760b51c000d71482eb0096168
Daniel Bispo <szagot@gmail.com>
2019-07-26 13:25:44 -0300

    Corrigindo view do curso para adicionar página 404

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 
@@ -11,7 +11,12 @@ def index(request):
 
 
 def details(request, course_id):
-    curso = Course.objects.get(pk=course_id)
+    # Esse método funciona, mas pode retornar erro se o ID não existir.
+    # curso = Course.objects.get(pk=course_id)
+
+    # O ideal é usar assim, para direcionar à pagina 404 se o objeto não existir:
+    curso = get_object_or_404(Course, pk=course_id)
+
     return render(request, 'courses/detalhe.html', {
         'course': curso
     })
