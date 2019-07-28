b7977b9e68f5b753bc0fdc285a468342ac4cf782
Daniel Bispo <szagot@gmail.com>
2019-07-28 20:19:02 -0300

    Criado view e template para cadastro básico

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from django.contrib.auth.views import LoginView
+from . import views
 
 # Definindo namespace
 app_name = 'accounts'
@@ -8,4 +8,6 @@ app_name = 'accounts'
 urlpatterns = [
     # Usando a view do próprio Django para Login de usuários (não tem a ver com o ADMIN)
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+
+    path('cadastro/', views.register, name='register'),
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
-# Create your views here.
+
+def register(request):
+    """
+    View para cadastro de novo usuário
+    """
+    # Verificando postagem
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        # Formulário válido?
+        if form.is_valid():
+            # Salva os dados do form
+            form.save()
+            # Redireciona para a URL de login
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
