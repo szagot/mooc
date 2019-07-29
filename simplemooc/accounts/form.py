984a6f0d15ff1991e7054531a8b7f2a6546c1d25
Daniel Bispo <szagot@gmail.com>
2019-07-29 10:02:25 -0300

    Fazendo verificações no campo email antes de salvar

--- simplemooc/accounts/form.py
+++ simplemooc/accounts/form.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
@@ -7,6 +8,18 @@ class RegisterForm(UserCreationForm):
     Personalizando o formulário padrão (UserCreationForm) para cadastro de usuário
     """
     email = forms.EmailField(label='E-mail')
+
+    def clean_email(self):
+        """
+        Toda função clean_<campo>() é executada automaticamente para fazer verificações antes de salvar
+        """
+        email = self.cleaned_data['email']
+        # Verifica se o email já existe em outro usuário
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Já existe um usuário com esse email')
+
+        # Se não existe duplicações, retorna o email
+        return email
 
     def save(self, commit=True):
         """
